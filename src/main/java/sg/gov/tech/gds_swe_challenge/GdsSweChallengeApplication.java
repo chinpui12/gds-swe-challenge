678a935cb6a75f651a7bfe03b28cb2b1504196cc
Chinpui <chinpui.chai@gmail.com>
2025-12-25 14:08:25 +0800

    feat: initial project setup

---
+++ src/main/java/sg/gov/tech/gds_swe_challenge/GdsSweChallengeApplication.java
@@ -0,0 +1,13 @@
+package sg.gov.tech.gds_swe_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdsSweChallengeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GdsSweChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/gov/tech/gds_swe_challenge/GdsSweChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.gov.tech.gds_swe_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdsSweChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
